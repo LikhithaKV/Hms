88c0a06fb30733b8c3655b870d41859624b27901
LENOVO <LENOVO@DESKTOP-2L9NM0M>
2020-12-17 07:54:02 +0530

    pushing only to branch2

--- src/test/java/liki/Liki.java
+++ src/test/java/liki/Liki.java
@@ -3,7 +3,7 @@ package liki;
 public class Liki {
 
 	public static void main(String[] args) {
-		System.out.println("updating it to branch1");
+		System.out.println("updating it to branch2 man");
 
 	}
 
